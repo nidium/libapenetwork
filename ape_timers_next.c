dc286e8c79a38a544b4c375a56681c2a01f9e10b
paraboul <a.catel@weelya.com>
2013-10-29 00:21:58 +0100

    Indentation

--- ape_timers_next.c
+++ ape_timers_next.c
@@ -89,74 +89,74 @@ clock_gettime(int X, struct timeval *tv)
 }
 static __inline uint64_t mach_absolute_time()
 {
-	struct timeval t;
-	clock_gettime(0, &t);
+    struct timeval t;
+    clock_gettime(0, &t);
 
-	return ((uint64_t)t.tv_sec * 1000000 + (uint64_t)t.tv_usec)*1000;
+    return ((uint64_t)t.tv_sec * 1000000 + (uint64_t)t.tv_usec)*1000;
 }
 #else // !__WIN32
 static __inline uint64_t mach_absolute_time()
 {
-	struct timespec t;
-	clock_gettime(CLOCK_MONOTONIC, &t);
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
 
-	return (uint64_t)t.tv_sec * 1000000000 + (uint64_t)t.tv_nsec;
+    return (uint64_t)t.tv_sec * 1000000000 + (uint64_t)t.tv_nsec;
 }
 #endif
 #endif
 
 int process_timers(ape_timers *timers)
 {
-	ape_timer *cur = timers->head;
+    ape_timer *cur = timers->head;
     uint64_t inums = UINT64_MAX, lastsample = 0;
 
-	/* TODO: paused timer */
-	while (cur != NULL) {
-		uint64_t start;
+    /* TODO: paused timer */
+    while (cur != NULL) {
+        uint64_t start;
 
-		if (cur->flags & APE_TIMER_IS_CLEARED) {
-			cur = del_timer(timers, cur);
-			continue;
-		}
+        if (cur->flags & APE_TIMER_IS_CLEARED) {
+            cur = del_timer(timers, cur);
+            continue;
+        }
 
-		if ((start = mach_absolute_time()) >= cur->schedule-150000) {
-			uint64_t ret;
-			unsigned int duration;
-			
-			ret = cur->callback(cur->arg);
+        if ((start = mach_absolute_time()) >= cur->schedule-150000) {
+            uint64_t ret;
+            unsigned int duration;
+            
+            ret = cur->callback(cur->arg);
 
             printf("Timer returned %lld\n", ret);
 
-			if (ret == -1) {
-				cur->schedule = start + cur->ticks_needs;
-			} else if (ret == 0) {
-				cur = del_timer(timers, cur);
-				continue;
-			} else {
-				cur->ticks_needs = ret * 1000000;
-				cur->schedule = start + cur->ticks_needs;
-			}
+            if (ret == -1) {
+                cur->schedule = start + cur->ticks_needs;
+            } else if (ret == 0) {
+                cur = del_timer(timers, cur);
+                continue;
+            } else {
+                cur->ticks_needs = ret * 1000000;
+                cur->schedule = start + cur->ticks_needs;
+            }
 
             lastsample = mach_absolute_time();
-			duration = lastsample - start;
+            duration = lastsample - start;
 
-			if (cur->stats.max < duration / 1000000) {
-				cur->stats.max = duration / 1000000;
-			}
-			if (cur->stats.min == 0 || duration / 1000000 < cur->stats.min) {
-				cur->stats.min = duration / 1000000;
-			}
-			cur->stats.nexec++;
-			cur->stats.totaltime += duration / 1000000;
+            if (cur->stats.max < duration / 1000000) {
+                cur->stats.max = duration / 1000000;
+            }
+            if (cur->stats.min == 0 || duration / 1000000 < cur->stats.min) {
+                cur->stats.min = duration / 1000000;
+            }
+            cur->stats.nexec++;
+            cur->stats.totaltime += duration / 1000000;
 
-		}
+        }
 
         if (cur->schedule < inums) {
             inums = cur->schedule;
         }
 
-		cur = cur->next;
-	}
+        cur = cur->next;
+    }
 
     if (inums == UINT64_MAX) {
         return APE_TIMER_RESOLUTION;
@@ -168,122 +168,122 @@ int process_timers(ape_timers *timers)
 
     //printf("Next timer in : %lld or %d\n", inums-lastsample,  ape_max(1, (int)((inums-lastsample+500000)/1000000)));
 
-	return ape_max(1, (int)((inums-lastsample+500000)/1000000));
+    return ape_max(1, (int)((inums-lastsample+500000)/1000000));
 }
 
 ape_timer *get_timer_by_id(ape_timers *timers, int identifier)
 {
-	ape_timer *cur;
-	for (cur = timers->head; cur != NULL; cur = cur->next) {
-		if (cur->identifier == identifier) {
-			return cur;
-		}
-	}
-	return NULL;
+    ape_timer *cur;
+    for (cur = timers->head; cur != NULL; cur = cur->next) {
+        if (cur->identifier == identifier) {
+            return cur;
+        }
+    }
+    return NULL;
 }
 
 void clear_timer_by_id(ape_timers *timers, int identifier, int force)
 {
-	ape_timer *cur;
-	for (cur = timers->head; cur != NULL; cur = cur->next) {
-		if (cur->identifier == identifier) {
-			if (!(cur->flags & APE_TIMER_IS_PROTECTED) ||
-				(cur->flags & APE_TIMER_IS_PROTECTED && force)) {
-				
-				cur->flags |= APE_TIMER_IS_CLEARED;
-			}
-			return;
-		}
-	}	
+    ape_timer *cur;
+    for (cur = timers->head; cur != NULL; cur = cur->next) {
+        if (cur->identifier == identifier) {
+            if (!(cur->flags & APE_TIMER_IS_PROTECTED) ||
+                (cur->flags & APE_TIMER_IS_PROTECTED && force)) {
+                
+                cur->flags |= APE_TIMER_IS_CLEARED;
+            }
+            return;
+        }
+    }   
 }
 
 void del_timers_unprotected(ape_timers *timers)
 {
-	ape_timer *cur = timers->head;
+    ape_timer *cur = timers->head;
 
-	while (cur != NULL) {
-		if (!(cur->flags & APE_TIMER_IS_PROTECTED)) {
-			cur = del_timer(timers, cur);
-			continue;
-		}
+    while (cur != NULL) {
+        if (!(cur->flags & APE_TIMER_IS_PROTECTED)) {
+            cur = del_timer(timers, cur);
+            continue;
+        }
 
-		cur = cur->next;
-	}
+        cur = cur->next;
+    }
 }
 
 /* delete *timer* and returns *timer->next* */
 ape_timer *del_timer(ape_timers *timers, ape_timer *timer)
 {
-	ape_timer *ret;
+    ape_timer *ret;
 
-	if (timer->prev == NULL) {
-		timers->head = timer->next;
-	} else {
-		timer->prev->next = timer->next;
-	}
-	if (timer->next != NULL) {
-		timer->next->prev = timer->prev;
-	}
-	ret = timer->next;
+    if (timer->prev == NULL) {
+        timers->head = timer->next;
+    } else {
+        timer->prev->next = timer->next;
+    }
+    if (timer->next != NULL) {
+        timer->next->prev = timer->prev;
+    }
+    ret = timer->next;
 
-	if (timer->clearfunc) {
-		timer->clearfunc(timer->arg);
-	}
-	
-	free(timer);
+    if (timer->clearfunc) {
+        timer->clearfunc(timer->arg);
+    }
+    
+    free(timer);
 
-	return ret;
+    return ret;
 }
 
 void timer_stats_print(ape_timer *timer)
 {
-	printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", timer->identifier,
-		timer->stats.nexec,
-		timer->stats.totaltime,
-		timer->stats.max,
-		timer->stats.min,
-		(timer->stats.nexec == 0 ? timer->stats.totaltime :
-			timer->stats.totaltime/timer->stats.nexec));
+    printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", timer->identifier,
+        timer->stats.nexec,
+        timer->stats.totaltime,
+        timer->stats.max,
+        timer->stats.min,
+        (timer->stats.nexec == 0 ? timer->stats.totaltime :
+            timer->stats.totaltime/timer->stats.nexec));
 }
 
 void timers_stats_print(ape_timers *timers)
 {
-	ape_timer *cur;
-	printf("=======================\n");
-	printf("Id\t\ttimes\texec\t\tmax\t\tmin\t\tavg\n");
-	for (cur = timers->head; cur != NULL; cur = cur->next) {
-		timer_stats_print(cur);
-	}
-	printf("=======================\n");
+    ape_timer *cur;
+    printf("=======================\n");
+    printf("Id\t\ttimes\texec\t\tmax\t\tmin\t\tavg\n");
+    for (cur = timers->head; cur != NULL; cur = cur->next) {
+        timer_stats_print(cur);
+    }
+    printf("=======================\n");
 }
 
 ape_timer *add_timer(ape_timers *timers, int ms, timer_callback cb, void *arg)
 {
-	ape_timer *timer = (ape_timer *)malloc(sizeof(ape_timer));
+    ape_timer *timer = (ape_timer *)malloc(sizeof(ape_timer));
 
     printf("Timer added : %d\n", ms);
 
-	timers->last_identifier++;
-	timer->callback = cb;
-	timer->ticks_needs = (uint64_t)ms * 1000000LL;
-	timer->schedule = mach_absolute_time() + timer->ticks_needs;
-	timer->arg = arg;
-	timer->flags = APE_TIMER_IS_PROTECTED;
-	timer->prev = NULL;
-	timer->identifier = timers->last_identifier;
-	timer->next = timers->head;
-	timer->clearfunc = NULL;
+    timers->last_identifier++;
+    timer->callback = cb;
+    timer->ticks_needs = (uint64_t)ms * 1000000LL;
+    timer->schedule = mach_absolute_time() + timer->ticks_needs;
+    timer->arg = arg;
+    timer->flags = APE_TIMER_IS_PROTECTED;
+    timer->prev = NULL;
+    timer->identifier = timers->last_identifier;
+    timer->next = timers->head;
+    timer->clearfunc = NULL;
 
-	timer->stats.nexec = 0;
-	timer->stats.totaltime = 0;
-	timer->stats.max = 0;
-	timer->stats.min = 0;
+    timer->stats.nexec = 0;
+    timer->stats.totaltime = 0;
+    timer->stats.max = 0;
+    timer->stats.min = 0;
 
-	if (timers->head != NULL) {
-		timers->head->prev = timer;
-	}
+    if (timers->head != NULL) {
+        timers->head->prev = timer;
+    }
 
-	timers->head = timer;
-	//printf("Timer added %d %p\n", ms, timers->head);
-	return timer;
+    timers->head = timer;
+    //printf("Timer added %d %p\n", ms, timers->head);
+    return timer;
 }
