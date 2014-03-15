2ac8940f9a5316286df84a8a89a61aeffbbf9e0b
paraboul <paraboul@gmail.com>
2014-03-15 11:18:09 +0100

    Indentation to space

--- ape_websocket.c
+++ ape_websocket.c
@@ -51,41 +51,41 @@ char *ape_ws_compute_key(const char *key, unsigned int key_len)
 }
 
 void ape_ws_write(ape_socket *socket_client, unsigned char *data,
-	size_t len, ape_socket_data_autorelease data_type)
+    size_t len, ape_socket_data_autorelease data_type)
 {
-	unsigned char payload_head[32] = { 0x81 };
-	size_t payload_length = 0;
+    unsigned char payload_head[32] = { 0x81 };
+    size_t payload_length = 0;
 
-	if (len <= 125) {
-		payload_head[1] = (unsigned char)len & 0x7F;
-		
-		payload_length = 2;
-	} else if (len <= 65535) {
-		unsigned short int s = htons(len);
-		payload_head[1] = 126;
-		memcpy(&payload_head[2], &s, 2);
-		
-		payload_length = 4;
-		
-	} else if (len <= 0xFFFFFFFF) {
-		unsigned int s = htonl(len);
-		payload_head[1] = 127;
-		payload_head[2] = 0;
-		payload_head[3] = 0;
-		payload_head[4] = 0;
-		payload_head[5] = 0;
-		
-		memcpy(&payload_head[6], &s, 4);
-		
-		payload_length = 10;
-	}
-	
-	printf("Writting WS to %d\n", socket_client->s.fd);
-	PACK_TCP(socket_client->s.fd);
-		APE_socket_write(socket_client, payload_head,
+    if (len <= 125) {
+        payload_head[1] = (unsigned char)len & 0x7F;
+        
+        payload_length = 2;
+    } else if (len <= 65535) {
+        unsigned short int s = htons(len);
+        payload_head[1] = 126;
+        memcpy(&payload_head[2], &s, 2);
+        
+        payload_length = 4;
+        
+    } else if (len <= 0xFFFFFFFF) {
+        unsigned int s = htonl(len);
+        payload_head[1] = 127;
+        payload_head[2] = 0;
+        payload_head[3] = 0;
+        payload_head[4] = 0;
+        payload_head[5] = 0;
+        
+        memcpy(&payload_head[6], &s, 4);
+        
+        payload_length = 10;
+    }
+    
+    printf("Writting WS to %d\n", socket_client->s.fd);
+    PACK_TCP(socket_client->s.fd);
+        APE_socket_write(socket_client, payload_head,
             payload_length, APE_DATA_STATIC);
-		APE_socket_write(socket_client, data, len, data_type);
-	FLUSH_TCP(socket_client->s.fd);
+        APE_socket_write(socket_client, data, len, data_type);
+    FLUSH_TCP(socket_client->s.fd);
 }
 
 void ape_ws_close(websocket_state *state)
@@ -99,10 +99,10 @@ void ape_ws_close(websocket_state *state)
 
 void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
 {
-	const buffer *buffer = &websocket->socket->data_in;
-	unsigned char *pData;
+    const buffer *buffer = &websocket->socket->data_in;
+    unsigned char *pData;
 
-	#if 1
+    #if 1
 
     for (pData = (unsigned char *)&buffer->data[websocket->offset];
         websocket->offset < buffer->used; websocket->offset++, pData++) {
@@ -114,7 +114,7 @@ void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
 
                 if (++websocket->key.pos == 4) {
                     websocket->step = WS_STEP_DATA;
-					          websocket->data_inkey = 0;
+                              websocket->data_inkey = 0;
                 }
                 break;
             case WS_STEP_START:
@@ -125,7 +125,7 @@ void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
             case WS_STEP_LENGTH:
                 /* Check for MASK bit */
                 if (!(*pData & 0x80)) {
-					//websocket->step = 
+                    //websocket->step = 
                     return;
                 }
                 switch (*pData & 0x7F) { /* 7bit length */
@@ -172,19 +172,19 @@ void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
                 if (websocket->data_pos == 0) {
                     websocket->data_pos = websocket->offset;
                     /* TODO: zerocopy - uncypher data in place */
-					websocket->data = (unsigned char *)malloc(sizeof(char) *
+                    websocket->data = (unsigned char *)malloc(sizeof(char) *
                         websocket->frame_payload.extended_length + 1);
                 }
                 
                 //*pData ^= websocket->key.val[websocket->data_inkey++ % 4];
-		        websocket->data[websocket->data_inkey] =
+                websocket->data[websocket->data_inkey] =
                     *pData ^ websocket->key.val[websocket->data_inkey % 4];
 
-		        websocket->data_inkey++;
-				
+                websocket->data_inkey++;
+                
                 if (--websocket->frame_payload.extended_length == 0) {
                     unsigned char saved;
-					websocket->data[websocket->data_inkey] = '\0';
+                    websocket->data[websocket->data_inkey] = '\0';
                     //websocket->data = &buffer->data[websocket->data_pos];
                     websocket->step = WS_STEP_START;
                     websocket->frame_pos = -1;
@@ -211,7 +211,7 @@ void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
                         }
                         case 0x9:
                         {
-							int body_length = /*&buffer->data[websocket->offset+1] - websocket->data;*/ 0;
+                            int body_length = /*&buffer->data[websocket->offset+1] - websocket->data;*/ 0;
                             char payload_head[2] = { 0x8a, body_length & 0x7F };
                             
                             /* All control frames MUST be 125 bytes or less */
@@ -233,15 +233,15 @@ void ape_ws_process_frame(websocket_state *websocket, ape_global *ape)
                             break;
                         default:
                             /* TODO handle binary/ASCII */
-							websocket->on_frame(websocket, websocket->data,
+                            websocket->on_frame(websocket, websocket->data,
                                 websocket->data_inkey, ape);
-							#if 0
+                            #if 0
                             /* Data frame */
                             saved = buffer->data[websocket->offset+1];
                             buffer->data[websocket->offset+1] = '\0';
                             //parser->onready(parser, g_ape);
                             buffer->data[websocket->offset+1] = saved;   
-                         	#endif
+                            #endif
                             break;
                     }
                     
--- ape_websocket.h
+++ ape_websocket.h
@@ -37,43 +37,43 @@ typedef struct _websocket_state
 {
     ape_socket *socket;
 
-	unsigned char *data;
-	void (*on_frame)(struct _websocket_state *,
+    unsigned char *data;
+    void (*on_frame)(struct _websocket_state *,
         const unsigned char *, ssize_t, ape_global *);
-	
-	unsigned int offset;
-	unsigned short int error;
-	//ws_version version;
     
-	struct {
-	    /* cypher key */
-	    unsigned char val[4];
-	    int pos;
-	} key;
+    unsigned int offset;
+    unsigned short int error;
+    //ws_version version;
+    
+    struct {
+        /* cypher key */
+        unsigned char val[4];
+        int pos;
+    } key;
     
     #pragma pack(2)
-	struct {
-	    unsigned char start;
-	    unsigned char length; /* 7 bit length */
-	    union {
-	        unsigned short short_length; /* 16 bit length */
-	        unsigned long long int extended_length; /* 64 bit length */
-	    };
-	} frame_payload;
-	#pragma pack()
-	
-	ws_payload_step step;
-	int data_pos;
-	int data_inkey;
-	int frame_pos;
-	int close_sent;
+    struct {
+        unsigned char start;
+        unsigned char length; /* 7 bit length */
+        union {
+            unsigned short short_length; /* 16 bit length */
+            unsigned long long int extended_length; /* 64 bit length */
+        };
+    } frame_payload;
+    #pragma pack()
+    
+    ws_payload_step step;
+    int data_pos;
+    int data_inkey;
+    int frame_pos;
+    int close_sent;
 } websocket_state;
 
 void ape_ws_process_frame(websocket_state *websocket, ape_global *ape);
 char *ape_ws_compute_key(const char *key, unsigned int key_len);
 void ape_ws_write(ape_socket *socket_client, unsigned char *data,
-	size_t len, ape_socket_data_autorelease data_type);
-	
+    size_t len, ape_socket_data_autorelease data_type);
+    
 void ape_ws_close(websocket_state *state);
 
 #define WEBSOCKET_HARDCODED_HEADERS "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n"
