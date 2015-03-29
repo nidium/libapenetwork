c6d47b02630573d668d7bb2e4bab391bc8205c07
Anthony Catel <paraboul@gmail.com>
2015-03-29 16:35:42 +0200

    removing debug

--- ape_events_loop.c
+++ ape_events_loop.c
@@ -62,7 +62,6 @@ void events_loop(ape_global *ape)
                     if (bitev & EVENT_READ) {
                         if (APE_SOCKET(attach)->states.proto == APE_SOCKET_PT_TCP ||
 							APE_SOCKET(attach)->states.proto == APE_SOCKET_PT_SSL) {
-							printf("Accept socket\n");
                             ape_socket_accept(APE_SOCKET(attach));
                         } else {
                             ape_socket_read_udp(APE_SOCKET(attach));
