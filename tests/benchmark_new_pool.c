97aa65239f84ad83ca28c1acdd677ebd9de92e9c
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2015-04-24 15:21:20 +0200

    Enhanced unittests for ape_pool.

---
+++ tests/benchmark_new_pool.c
@@ -0,0 +1,26 @@
+#include <ape_pool.h>
+
+#include <stdio.h>
+
+#define SIZE_STEP 16
+#define SIZE_LIMIT 256
+#define N_STEP 1
+#define N_LIMIT 512
+
+#define REPEAT 2048
+
+int main(const int argc, const char **argv )
+{
+	size_t repeat, size, n;
+	for(repeat = 0; repeat < REPEAT; repeat++) {
+		for(size = SIZE_STEP; size < SIZE_LIMIT; size += SIZE_STEP) {
+			for(n = N_STEP; n < N_LIMIT; n += N_STEP) {
+				//printf("%d %d %d\n", size, n, repeat);
+				ape_pool_t * p = ape_new_pool(size, n);
+				ape_destroy_pool(p);
+			}
+		}
+	}
+
+	return 0;
+}
--- tests/unittest_pool.cpp
+++ tests/unittest_pool.cpp
@@ -3,12 +3,72 @@
 #include <gtest/gtest.h>
 
 #include <ape_pool.h>
-/*
+
+TEST(Pool, Simple)
+{
+	ape_pool_t * pool;
+
+	pool = NULL;
+
+	pool = ape_new_pool( 16, 2 );
+	EXPECT_TRUE(pool != NULL);
+	EXPECT_TRUE(pool->next != NULL);
+	EXPECT_TRUE(pool->next->next == NULL);
+	EXPECT_TRUE(pool->prev == NULL);
+	EXPECT_TRUE(pool == pool->next->prev);
+	ape_destroy_pool( pool );
+}
+
+TEST(Pool, Looped)
+{
+#define SIZE_START 16
+#define SIZE_LIMIT 512
+#define SIZE_STEP 16 
+#define N_START 1
+#define N_LIMIT 128
+#define N_STEP 16 
+	ape_pool_t * pool;
+	size_t size, n;
+
+	pool = NULL;
+	pool = ape_new_pool(0, 1);
+	EXPECT_TRUE(pool != NULL);
+	ape_destroy_pool(pool);
+
+	pool = NULL;
+	pool = ape_new_pool(SIZE_START, 0);
+	EXPECT_TRUE(pool == NULL);
+
+	for( n = 0; n < N_START; n += N_STEP ) {
+		for(size = 1; size < SIZE_START; size += SIZE_STEP) {
+			pool = NULL;
+			pool = ape_new_pool(size, n);
+			EXPECT_TRUE(pool == NULL);
+		}
+	}
+
+	for( n = N_START; n < N_LIMIT; n += N_STEP ) {
+		for(size = SIZE_START; size < SIZE_LIMIT; size += SIZE_STEP) {
+			pool = NULL;
+			pool = ape_new_pool(size, n);
+			EXPECT_TRUE(pool != NULL);
+			ape_destroy_pool(pool);
+		}
+	}
+#undef SIZE_START
+#undef SIZE_LIMIT
+#undef SIZE_STEP
+#undef N_START
+#undef N_LIMIT
+#undef N_STEP
+}
+
 struct dummy_t {
+	ape_pool_t pool;
 	int a;
 	char * b;
 };
-
+#if 0
 static struct dummy_t * Dummy_New( int a, const char * b)
 {
 	struct dummy_t * dummy;
@@ -19,38 +79,56 @@ static struct dummy_t * Dummy_New( int a, const char * b)
 
 	return dummy;
 }
-
-static void Dummy_Delete( struct dummy_t *dummy)
+static void Dummy_Delete(struct dummy_t *dummy)
 {
 	dummy->a = 0;
 	free(dummy->b); dummy->b = NULL;
+	free(dummy); dummy = NULL;
 }
-*/
-TEST(Pool, Simple)
+
+
+#endif
+
+static void Dummy_Init( struct dummy_t * dummy)
 {
-	ape_pool_t * pool;
-
-	pool = NULL;
-
-	pool = ape_new_pool( 5, 20 );
-	EXPECT_TRUE(pool != NULL);
-	ape_destroy_pool( pool );
-}
-TEST(Pool, Looped)
-{
-#define SIZE_LIMIT 513
-#define N_LIMIT 260
-	ape_pool_t * pool;
-	size_t size, n;
-
-	for( n = 0; n < 33; n++ ); {
-		for( size = 0; size < 33; size++ ); {
-			pool = NULL;
-			pool = ape_new_pool( size, n );
-			EXPECT_TRUE(pool != NULL);
-			ape_destroy_pool( pool );
-		}
+	if (dummy) {
+		dummy->a = 0;
+		dummy->b = NULL;
 	}
-#undef SIZE_LIMIT
-#undef N_LIMIT
 }
+
+static void Dummy_Set( struct dummy_t * dummy, int a, const char * b)
+{
+	if (dummy) {
+		dummy->a = a;
+		if (dummy->b) {
+			free(dummy->b); dummy->b = NULL;
+		}
+		dummy->b = strdup(b);
+	}
+}
+
+static void Dummy_Clear( struct dummy_t * dummy)
+{
+	if (dummy) {
+		dummy->a = 0;
+		free(dummy->b);dummy->b = NULL;
+	}
+}
+
+TEST(Pool, allocate)
+{
+	ape_pool_t * pool;
+
+	pool = ape_new_pool(sizeof(struct dummy_t), 2);
+	Dummy_Init((struct dummy_t*) &pool->ptr.data);
+	Dummy_Init((struct dummy_t*) &pool->next->ptr.data);
+	Dummy_Set((struct dummy_t*) &pool->ptr.data, 1, "Nidium");
+	Dummy_Set((struct dummy_t*) &pool->next->ptr.data, 2, "A new breed of browser");
+	Dummy_Clear((struct dummy_t*) &pool->ptr.data);
+	Dummy_Clear((struct dummy_t*) &pool->next->ptr.data);
+	
+	ape_destroy_pool(pool);
+}
+
+
