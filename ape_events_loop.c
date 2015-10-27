nidium/libapenetwork

commit 974f685123ecef4c4bcc449dada52e315d513480

ape_events_loop: cleanup

ape_events_loop.c
@@ -48,80 +48,84 @@ void events_loop(ape_global *ape)
         }
 
         for (i = 0; i < nfd; i++) {
-            if ((evd  = events_get_current_evd(&ape->events, i)) == NULL) {
+            ape_socket *cursocket = APE_EVENT_SOCKET_PTR(evd);
+
+            if ((evd = events_get_current_evd(&ape->events, i)) == NULL) {
                 continue;
             }
             
-            bitev   = events_revent(&ape->events, i);
+            bitev = events_revent(&ape->events, i);
 
              /* assuming that ape_event_descriptor is the first member */
-            fd  = evd->fd;
+            fd = evd->fd;
 
             switch(evd->type) {
             case APE_EVENT_SOCKET:
-                if (APE_EVENT_SOCKET_PTR(evd)->states.type == APE_SOCKET_TP_SERVER) {
+                if (cursocket->states.type == APE_SOCKET_TP_SERVER) {
                     if (bitev & EVENT_READ) {
-                        if (APE_EVENT_SOCKET_PTR(evd)->states.proto == APE_SOCKET_PT_TCP ||
-                            APE_EVENT_SOCKET_PTR(evd)->states.proto == APE_SOCKET_PT_SSL) {
-                            ape_socket_accept(APE_EVENT_SOCKET_PTR(evd));
+                        if (cursocket->states.proto == APE_SOCKET_PT_TCP ||
+                            cursocket->states.proto == APE_SOCKET_PT_SSL) {
+                            ape_socket_accept(cursocket);
                         } else {
-                            ape_socket_read_udp(APE_EVENT_SOCKET_PTR(evd));
+                            ape_socket_read_udp(cursocket);
                         }
                     }
-                } else if (APE_EVENT_SOCKET_PTR(evd)->states.type == APE_SOCKET_TP_CLIENT) {
+                } else if (cursocket->states.type == APE_SOCKET_TP_CLIENT) {
                     /* unset this before READ event because read can invoke writes */
                     if (bitev & EVENT_WRITE) {
-                        APE_EVENT_SOCKET_PTR(evd)->states.flags &= ~APE_SOCKET_WOULD_BLOCK;
+                        cursocket->states.flags &= ~APE_SOCKET_WOULD_BLOCK;
                     }
 
-                    if (APE_EVENT_SOCKET_PTR(evd)->states.proto != APE_SOCKET_PT_UDP &&
+                    if (cursocket->states.proto != APE_SOCKET_PT_UDP &&
                         (bitev & EVENT_READ) &&
-                        ape_socket_read(APE_EVENT_SOCKET_PTR(evd)) == -1) {
+                        ape_socket_read(cursocket) == -1) {
                         
                         /* ape_socket is planned to be released after the for block */
                         continue;
-                    } else if (APE_EVENT_SOCKET_PTR(evd)->states.proto ==
+                    } else if (cursocket->states.proto ==
                         APE_SOCKET_PT_UDP && (bitev & EVENT_READ)) {
 
-                        ape_socket_read_udp(APE_EVENT_SOCKET_PTR(evd));
+                        ape_socket_read_udp(cursocket);
                     }
 
                     if (bitev & EVENT_WRITE) {
-                        if (APE_EVENT_SOCKET_PTR(evd)->states.state == APE_SOCKET_ST_ONLINE &&
-                                !(APE_EVENT_SOCKET_PTR(evd)->states.flags & APE_SOCKET_WOULD_BLOCK)) {
+                        if (cursocket->states.state == APE_SOCKET_ST_ONLINE &&
+                                !(cursocket->states.flags & APE_SOCKET_WOULD_BLOCK)) {
 
                             /*
                                 Execute the jobs list.
                                 If the job list is done (returns 1), call drain callback.
                             */
-                            if (ape_socket_do_jobs(APE_EVENT_SOCKET_PTR(evd)) == 1 &&
-                                APE_EVENT_SOCKET_PTR(evd)->callbacks.on_drain != NULL) {
-                                APE_EVENT_SOCKET_PTR(evd)->callbacks.on_drain(
-                                    APE_EVENT_SOCKET_PTR(evd), ape,
-                                    APE_EVENT_SOCKET_PTR(evd)->callbacks.arg
+                            printf("Do job...%d\n", (cursocket->states.events_flags & EVENT_WRITE));
+                            if (ape_socket_do_jobs(cursocket) == 1 &&
+                                cursocket->callbacks.on_drain != NULL) {
+                                cursocket->callbacks.on_drain(
+                                    cursocket, ape,
+                                    cursocket->callbacks.arg
                                 );
                             }
 
-                        } else if (APE_EVENT_SOCKET_PTR(evd)->states.state == APE_SOCKET_ST_PROGRESS) {
+                        } else if (cursocket->states.state == APE_SOCKET_ST_PROGRESS) {
                             int ret;
                             char serror = '\0';
                             socklen_t serror_len = sizeof(serror);
 
-                            if ((ret = getsockopt(fd, SOL_SOCKET, SO_ERROR, &serror, &serror_len)) == 0 &&
-                                serror == 0) {
+                            if ((ret = getsockopt(fd, SOL_SOCKET, SO_ERROR,
+                                    &serror, &serror_len)) == 0 &&
+                                    serror == 0) {
                                 
-                                APE_EVENT_SOCKET_PTR(evd)->states.state = APE_SOCKET_ST_ONLINE;
+                                cursocket->states.state = APE_SOCKET_ST_ONLINE;
 
-                                ape_socket_connected(APE_EVENT_SOCKET_PTR(evd));
+                                ape_socket_connected(cursocket);
                                 
                             } else {
-                                ape_socket_destroy(APE_EVENT_SOCKET_PTR(evd));
+                                ape_socket_destroy(cursocket);
                             }
                         }
                     }
-                } else if (APE_EVENT_SOCKET_PTR(evd)->states.type == APE_SOCKET_TP_UNKNOWN) {
+                }/* else if (cursocket->states.type == APE_SOCKET_TP_UNKNOWN) {
 
-                }
+                }*/
 
                 break;
             case APE_EVENT_DELEGATE:

commit 34ec9dd2f0590081d7825e6eaa7e0760b2ae4207

Fix bug introduced in 974f685123ecef4c4bcc449dada52e315d513480

ape_events_loop.c
@@ -40,6 +40,7 @@ void events_loop(ape_global *ape)
     //uint64_t start_monotonic = mach_absolute_time(), end_monotonic;
 
     while(ape->is_running && ape_running) {
+        ape_socket *cursocket;
         int i;
 
         events_shrink(&ape->events);
@@ -48,11 +49,11 @@ void events_loop(ape_global *ape)
         }
 
         for (i = 0; i < nfd; i++) {
-            ape_socket *cursocket = APE_EVENT_SOCKET_PTR(evd);
-
             if ((evd = events_get_current_evd(&ape->events, i)) == NULL) {
                 continue;
             }
+
+            cursocket = APE_EVENT_SOCKET_PTR(evd);
             
             bitev = events_revent(&ape->events, i);
 