8d95d08af3c8e9bd9a25a64f60c590c238dac73d
Peter Reijnders <peter.reijnders@verpeteren.nl>
2017-02-02 09:54:33 +0100

    Repaired unittests

--- tests/unittest_logging.cpp
+++ tests/unittest_logging.cpp
@@ -12,44 +12,49 @@
 #include <ape_netlib.h>
 #include <ape_logging.h>
 
-static void loglog(void * cb_args, ape_log_lvl_t lvl, const char * lvl_label, const char * tag,
-    const char * fmt, va_list args)
-{
-    FILE * file_h;
+struct args {
+    FILE *file_h;
+};
 
-    file_h = (FILE*) cb_args;
-    if (file_h) {
+
+static void loglog(void * cb_args, ape_log_lvl_t lvl, const char * lvl_label, const char * tag,
+    const char * buffer )
+{
+    struct args * args;
+
+    args = (struct args*) cb_args;
+    if (args->file_h) {
         int datelen;
         char date[32];
         time_t log_ts;
 
         log_ts = time(NULL);
         datelen = strftime(&date[0], 32, "%Y-%m-%d %H:%M:%S - ", localtime(&log_ts));
-        fprintf(file_h, "%s\t", date);
-        fprintf(file_h, fmt, args);
-        fprintf(file_h, "\n");
+        fprintf(args->file_h, "%s\t%s\n", date, buffer);
     }
 }
 
 static void loginit(void * cb_args)
 {
-    FILE * file_h;
+    struct args * args;
     const char * tmp_file = "/tmp/test_ape_logging.log";
 
-    file_h = (FILE*) cb_args;
-    if (! file_h) {
-        file_h = fopen(tmp_file, "wb");
+    args = (struct args*) cb_args;
+    if (! args->file_h) {
+        args->file_h = fopen(tmp_file, "wb");
     }
-    fprintf(file_h, "writing stuff\n");
+    fprintf(args->file_h, "writing stuff\n");
 }
 
 static void logclear (void * cb_args)
 {
-    FILE * file_h;
+    struct args * args;
 
-    file_h = (FILE*) cb_args;
-    if (file_h) {
-        fclose(file_h);
+    args = (struct args*) cb_args;
+    if (args->file_h) {
+        fprintf(args->file_h, "clearing log\n");
+        fflush(args->file_h);
+        fclose(args->file_h);
     }
 }
 
@@ -58,22 +63,26 @@ TEST(Logger,  Logger)
     ape_global *g_ape;
     g_ape = APE_init();
     ape_logger_t logger;
-    FILE *file_h;
+    struct args args;
     int fwd;
 
-    file_h = NULL;
+    args.file_h = NULL;
     memset(&logger, 0, sizeof(logger));
-    APE_SetLogger(&logger, APE_Log_error, loginit, loglog, logclear, file_h);
+    APE_SetLogger(&logger, APE_LOG_ERROR, loginit, loglog, logclear, &args);
 
-    EXPECT_TRUE(logger.lvl == APE_Log_error);
+    EXPECT_TRUE(logger.lvl == APE_LOG_ERROR);
     EXPECT_TRUE(logger.init == loginit);
     EXPECT_TRUE(logger.log == loglog);
     EXPECT_TRUE(logger.clear == logclear);
-    EXPECT_TRUE(logger.cb_args == file_h);
+    EXPECT_TRUE(logger.cb_args == &args);
 
-    fwd = APE_Log(&logger, APE_Log_error, "tag," "should %s print", "");
+    fwd = APE_Logf(&logger, APE_LOG_ERROR, "tag", "should %s print", "indeed");
     EXPECT_TRUE(fwd == 1);
-    fwd = APE_Log(&logger, APE_Log_info, "tag," "should %s print", "not");
+    fwd = APE_Logf(&logger, APE_LOG_INFO, "tag", "should %s print", "not");
+    EXPECT_TRUE(fwd == 0);
+    fwd = APE_Log(&logger, APE_LOG_ERROR, "tag", "should print");
+    EXPECT_TRUE(fwd == 1);
+    fwd = APE_Log(&logger, APE_LOG_INFO, "tag", "should not print");
     EXPECT_TRUE(fwd == 0);
 
     logger.clear(logger.cb_args);
