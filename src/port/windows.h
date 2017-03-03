c56edb7f42a78cd2a24c38187fa029c8ce233ffb
peter <peter.reijnders@verpeteren.nl>
2017-03-03 09:23:29 +0100

    windows-x86: added macros for S_ISDIR and S_ISREG
    Problem: S_ISDIR is not available on the msvc platform

--- src/port/windows.h
+++ src/port/windows.h
@@ -31,6 +31,14 @@ typedef const char sockopt_t;
 
 #define _INC_ERRNO
 
+#ifndef S_ISDIR
+#define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
+#endif
+
+#ifndef S_ISREG
+#define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
+#endif
+
 #undef EBADF /* override definition in errno.h */
 #define EBADF WSAEBADF
 #undef EINTR /* override definition in errno.h */
