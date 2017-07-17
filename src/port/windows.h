4e700ba2c713aff6043b59e3b00e01a9bae1b058
efyx <n.trani@weelya.com>
2017-07-17 17:35:26 +0200

    Windows: Fix usleep method

--- src/port/windows.h
+++ src/port/windows.h
@@ -28,7 +28,18 @@ extern "C" {
 #endif
 
 #ifndef usleep
-#define usleep Sleep
+static void usleep(__int64 usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER ft;
+
+	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
 #endif
 
 #define strncasecmp _strnicmp
